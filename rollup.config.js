a38e8db24e1afe803b3ef25cfd5b6056e27c490d
Koutaro Mukai <mukai.k1011k@outlook.jp>
2025-06-08 17:49:35 +0900

    Setup web entrypoint

---
+++ rollup.config.js
@@ -0,0 +1,82 @@
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import alias from "@rollup/plugin-alias";
+import copy from "rollup-plugin-copy";
+import path from "path";
+
+export default [
+  {
+    input: "src/index.js",
+    output: {
+      file: "dist/index.js",
+      format: "es",
+      sourcemap: true,
+    },
+    plugins: [
+      alias({
+        entries: [
+          {
+            find: "jimp",
+            replacement: path.resolve(
+              "node_modules/jimp/dist/browser/index.js",
+            ),
+          },
+          {
+            find: "web-worker",
+            replacement: path.resolve(
+              "node_modules/web-worker/dist/browser/index.cjs",
+            ),
+          },
+        ],
+      }),
+      nodeResolve(),
+      commonjs(),
+      copy({
+        targets: [
+          {
+            src: "node_modules/mupdf/dist/mupdf-wasm.wasm",
+            dest: "dist",
+          },
+          {
+            src: "src/index.html",
+            dest: "dist",
+          },
+        ],
+      }),
+    ],
+  },
+  {
+    input: "src/worker.js",
+    output: {
+      file: "dist/worker.js",
+      format: "es",
+      sourcemap: true,
+    },
+    plugins: [
+      alias({
+        entries: [
+          {
+            find: "jimp",
+            replacement: path.resolve(
+              "node_modules/jimp/dist/browser/index.js",
+            ),
+          },
+        ],
+      }),
+      nodeResolve(),
+      commonjs(),
+      copy({
+        targets: [
+          {
+            src: "node_modules/mupdf/dist/mupdf-wasm.wasm",
+            dest: "dist",
+          },
+          {
+            src: "src/index.html",
+            dest: "dist",
+          },
+        ],
+      }),
+    ],
+  },
+];
